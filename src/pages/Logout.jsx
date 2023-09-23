070e591ab65c3ebb5769691a335d510111daba74
notamit <90555965+amitjimiwal@users.noreply.github.com>
2023-09-23 15:01:34 +0530

    chatbot test

--- src/pages/Logout.jsx
+++ src/pages/Logout.jsx
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from "react-redux"
 import { logoutAction } from "../reduxstore/slices/userSlice";
+import { useEffect } from "react";
 const Logout = () => {
   const dispatch=useDispatch();
   const handleLogout=async()=>{
@@ -8,6 +9,19 @@ const Logout = () => {
     }))
   }
       const {username}=useSelector(state=>state.user)
+      useEffect(()=>{
+        window.botpressWebChat.init({
+          "composerPlaceholder": "Sigma Bot",
+          "botConversationDescription": "Ask any Doubts regarding Mining Acts",
+          "botId": "7d830d80-b257-4e08-ad91-28604776db81",
+          "hostUrl": "https://cdn.botpress.cloud/webchat/v1",
+          "messagingUrl": "https://messaging.botpress.cloud",
+          "clientId": "7d830d80-b257-4e08-ad91-28604776db81",
+          "lazySocket": true,
+          "frontendVersion": "v1",
+          "showPoweredBy": true
+      });
+      },[])
   return (
     <div className="flex flex-col justify-center ">
       <h1 className="text-3xl mb-4 text-center">Welcome <span className="text-red-900">{username}</span></h1>
